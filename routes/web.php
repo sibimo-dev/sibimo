599f2a3d38f8731eefc69b15948c5daf21de17b2
Ghiska Arna <ghiskaramadani1510@gmail.com>
2026-09-22 21:48:09 +0700

    Add routes for new letter templates

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Services\LetterPdfService;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/letters/preview/{template}', function (string $template, LetterPdfService $service) {
+    $data = $service->sampleViewData($template);
+
+    return $service->previewHtml("letters.{$template}", $data);
+});
+
+Route::get('/letters/pdf/{template}', function (string $template, LetterPdfService $service) {
+    return $service->pdf("letters.{$template}", $service->sampleViewData($template))
+        ->stream("{$template}.pdf");
 });
